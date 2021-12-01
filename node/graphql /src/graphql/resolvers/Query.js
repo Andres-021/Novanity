34a054f4b1727332929a3b267a11c8e8a6763995
pilar <ingeniera.coral@gmail.com>
2021-11-30 21:29:40 -0500

    se elimina archivo graphql

--- node/graphql /src/graphql/resolvers/Query.js
+++
@@ -1,8 +0,0 @@
-import Usuario from "../../models/Usuario.js";
-const Query = {
-  usuarios: async () => {
-    return await Usuario.find();
-  },
-};
-
-export default Query;
--- node/graphql /src/index.js
+++
@@ -1,6 +0,0 @@
-import { server } from "./server.js";
-import "./database.js";
-
-server.start({ port: 4000 }, ({ port }) => {
-  console.log("El servicio está em el puerto", port);
-});
