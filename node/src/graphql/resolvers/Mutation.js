ca45c6b0060d4183619cbf759000728ef8814ec8
Andres Felipe <andresfelipequintero2091@gmail.com>
2021-12-06 19:48:52 -0500

    añadiendo los elementos restandes a editar cuando el usuario modifique su perfil. estos seran modificados donde el usuario tenga participacion.

--- node/src/graphql/resolvers/Mutation.js
+++ node/src/graphql/resolvers/Mutation.js
@@ -1,73 +1,72 @@
 import Usuario from "../../models/Usuario.js";
+import Proyecto from "../../models/Proyecto.js";
 
 const Mutation = {
+  login: async(_,{content}) => {
+    try{
+      // Se hace la busqueda del usuario a registrar en caso de encontrarlo retornamos los valores
+      return await Usuario.findOne({
+        "correo": content.correo,
+        "contrasena": content.contrasena
+      });
+      // console.log(res);
+    }catch(e){
+      return e;
+    }
+  },
   // Resolver de createUsuario en el schema para realizar el registro
   // En el content estarian todos los datos de usuario
   createUsuario: async (_,{content}) => {
     try{
-
+      // Creamos un nuevo usuario pasandolo al schema que sera almacenado en una variable y
+      // luego lo guardamos el la base de datos con .save()
       const newUsuario = new Usuario(content);
       return await newUsuario.save();
-    }catch{
+    }catch(e){
+      return e;
     }
   },
-
-  updateUsuario: async (
-    _,
-    { _id, cedula, nombre, correo, contrasena, rol, estado }
-  ) => {
-    const updateUsuario = {
-      cedula,
-      nombre,
-      correo,
-      contrasena,
-      rol,
-      estado
-    }
-    // Usuario.updateOne(id, {
-    //   $push:{
-    //     "avances":{
-    //       $each:[{
-    //         "descripcion":"saque a paser al perro",
-    //         "descripcion":"saque a paser al perro",
-    //         "descripcion":"saque a paser al perro"
-    //       }]
-    //     }
-    //   }
-    // });
-    return await Usuario.findByIdAndUpdate(_id, updateUsuario);
-  },
-
-  // En caso de usar content
-  // updateUsuario: async (_,{_id, content}) => {
-  //   return await Usuario.findByIdAndUpdate(_id, content);
-  // }
 
   editUsuario: async (root, args) => {
     //se crea mutacion para editar uno o varios datos de un registro de la coleccion usuarios ingresando el id -> se agregan los parametros en el schema
+    try{
+      const usuario = await Usuario.findOne({ _id: args._id });
 
-    const usuario = await Usuario.findOne({ _id: args._id });
-    if (args.nombre) {
-      usuario.nombre = args.nombre;
-    }
-    if (args.cedula) {
-      usuario.cedula = args.cedula;
-    }
-    if (args.correo) {
-      usuario.correo = args.correo;
-    }
+      if (args.nombre) {
+        usuario.nombre = args.nombre;
+      }
+      if (args.cedula) {
+        usuario.cedula = args.cedula;
+      }
+      if (args.correo) {
+        usuario.correo = args.correo;
+      }
 
-    if (args.contrasena) {
-      usuario.contrasena = args.contrasena;
+      if (args.contrasena) {
+        usuario.contrasena = args.contrasena;
+      }
+      if (args.rol) {
+        usuario.rol = args.rol;
+      }
+      if (args.estado) {
+        usuario.estado = args.estado;
+      }
+      
+      // Tomamos el id del usuario que actualizo su perfil y lo buscamos en proyectos participados.
+      // Luego en proyecto actualizamos el dato que cambio el usuario o edito en su perfil.
+      await Proyecto.findOneAndUpdate({"id_lider": args._id}, {"nombre_lider": args.nombre});
+      await Proyecto.findOneAndUpdate(
+        {'avances.id_estudiante': args._id}, // Buscamos por el id_estudiante en avances
+        {$set:{'avances.$[elem].nombre_estudiante': args.nombre}},{ // En todos los elemtos de nombre_e actualizamos por args.nombre
+        arrayFilters:[{'elem.id_estudiante': args._id}]
+      });
+      await Proyecto.findOneAndUpdate()
+
+      return await usuario.save();
+    }catch(e){
+      return e;
     }
-    if (args.rol) {
-      usuario.rol = args.rol;
-    }
-    if (args.estado) {
-      usuario.estado = args.estado;
-    }
-    return usuario.save();
-  },
+  }
 };
 
 export default Mutation;
